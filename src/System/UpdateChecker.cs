dfe30d920dae56473bf1199c9e78bc755fc9b50d
Diorser <tueur2010@gmail.com>
2025-12-27 05:38:26 +0800

    refactor(UpdateChecker): 调整版本检查源和镜像源的顺序
    将Gitee源调整至国内源优先位置，优化国内用户更新体验

--- src/System/UpdateChecker.cs
+++ src/System/UpdateChecker.cs
@@ -30,13 +30,15 @@ namespace LiteMonitor
         // ========================================================
         private static readonly string[] VersionJsonUrls =
         {
-            // 国内源
+            // Gitee RAW（自动 fallback 使用）
+             "https://gitee.com/Diorser/LiteMonitor/raw/master/resources/version.json",
+
+            // 国官网源
             "https://litemonitor.cn/update/version.json",
 
             // GitHub RAW（自动 fallback 使用）
              "https://raw.githubusercontent.com/Diorser/LiteMonitor/master/resources/version.json",
-             // Gitee RAW（自动 fallback 使用）
-             "https://gitee.com/Diorser/LiteMonitor/raw/master/resources/version.json",
+             
         };
 
         // ========================================================
@@ -44,13 +46,13 @@ namespace LiteMonitor
         // ========================================================
         private static readonly string[] Mirrors =
         {
-
+            
+            // Gitee Releases
+            "https://gitee.com/Diorser/LiteMonitor/releases/download/v{0}/LiteMonitor_v{0}-win-x64.zip",
             // 国内 CDN
             "https://litemonitor.cn/update/LiteMonitor_v{0}-win-x64.zip",
             // Github Releases
             "https://github.com/Diorser/LiteMonitor/releases/download/v{0}/LiteMonitor_v{0}-win-x64.zip",
-            // Gitee Releases
-            "https://gitee.com/Diorser/LiteMonitor/releases/download/v{0}/LiteMonitor_v{0}-win-x64.zip",
 
             
         };
